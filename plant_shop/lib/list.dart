266480708e32a57c84793d26fedca9f7c72cce3d
AliBahaa <alibahaa963@gmail.com>
2022-10-29 15:36:35 +0300

    search_field

--- plant_shop/lib/list.dart
+++ plant_shop/lib/list.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-class plant_list {
-  final String img , Name ,id , price;
-  plant_list(
-      {
-        required this.Name,
-        required this.id,
-        required this.img,
-        required this.price
 
-      });
+class plant_list {
+  final String img, Name, id, price;
+  plant_list(
+      {required this.Name,
+      required this.id,
+      required this.img,
+      required this.price});
 }
 
-List<plant_list> cart_lists =[
-
-  plant_list(id: 'c1',img:'assest/images/plant2.png',Name: 'Monstera', price: '\$39'),
-  plant_list(id: 'c2',img:'assest/images/plant3.png',Name: 'Ageratum', price: '\$18'),
-  plant_list(id: 'c3',img:'assest/images/plant4.png',Name: 'gester', price: '\$40'),
-
+List<plant_list> cart_lists = [
+  plant_list(
+      id: 'c1',
+      img: 'assest/images/plant2.png',
+      Name: 'Monstera',
+      price: '\$39'),
+  plant_list(
+      id: 'c2',
+      img: 'assest/images/plant3.png',
+      Name: 'Ageratum',
+      price: '\$18'),
+  plant_list(
+      id: 'c3', img: 'assest/images/plant4.png', Name: 'gester', price: '\$40'),
 ];
 
 class cartPlant extends StatelessWidget {
@@ -25,56 +30,54 @@ class cartPlant extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: EdgeInsets.only(bottom: 40),
       child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 5),
-
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color:  Color.fromARGB(255, 203, 200, 200)
-        ),
-        child: Container(
-          padding: EdgeInsets.all(5),
-          child: Column(
+          margin: EdgeInsets.symmetric(horizontal: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(255, 203, 200, 200)),
+          child: Container(
+            padding: EdgeInsets.all(5),
+            child: Column(
               children: [
-                Padding(padding: EdgeInsets.only(left:MediaQuery.of(context).size.width/3,
-                top: 5),
-                    child: Text(cart2.price,  style:const TextStyle(
-                        fontWeight: FontWeight.bold ))),
-              Container(
-                padding: EdgeInsets.only(top: 25),
-                  child: Image(image: AssetImage(cart2.img),height: 200,)
-              ),
-                Text(cart2.Name,style: const TextStyle (
-                    fontWeight: FontWeight.bold )),
-
+                Padding(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width / 3, top: 5),
+                    child: Text(cart2.price,
+                        style: const TextStyle(fontWeight: FontWeight.bold))),
+                Container(
+                    padding: EdgeInsets.only(top: 25),
+                    child: Image(
+                      image: AssetImage(cart2.img),
+                      height: 200,
+                    )),
+                Text(cart2.Name,
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 15),
-                  child: Row(
-                      children: [
+                  child: Row(children: [
                     Container(
                       padding: EdgeInsets.all(12),
                       decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(30)),
-                       child:const Text('Add to cart',style: TextStyle(
-                           fontWeight: FontWeight.bold )),
+                      child: const Text('Add to cart',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
                     ),
-                       const  SizedBox(width: 30,),
-                       const CircleAvatar(
-                          backgroundColor: Colors.black,
-                          child: Icon(Icons.favorite_outline_sharp,size: 20,color: Colors.white),
-                        )
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    const CircleAvatar(
+                      backgroundColor: Colors.black,
+                      child: Icon(Icons.favorite_outline_sharp,
+                          size: 20, color: Colors.white),
+                    )
                   ]),
                 ),
-          ],
-        ),
-      )
-      ),
+              ],
+            ),
+          )),
     );
-
-
-
   }
 }
--- plant_shop/lib/pages/home_page.dart
+++ plant_shop/lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'dart:developer';
 import 'dart:ffi';
 
@@ -8,7 +7,6 @@ import 'package:plant_shop/list.dart';
 import 'package:plant_shop/widgets/orginalButton.dart';
 
 class home_page extends StatefulWidget {
-
   @override
   State<home_page> createState() => _home_pageState();
 }
@@ -21,89 +19,102 @@ class _home_pageState extends State<home_page> {
       _selectedIndex = index;
     });
   }
+
   @override
   //final user =FirebaseAuth.instance.currentUser!;
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Stack(
-          children: [
-            Column(
-                children: [
-                  // SizedBox(
-                  //   //  height: MediaQuery.of(context).size.width / 4,
-                  // ),
-                  firstCart(context),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.width / 9,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Stack(children: [
+            Column(children: [
+              SizedBox(
+              height: MediaQuery.of(context).size.width / 10,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Find your \nfavorite plants',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25
                   ),
-                  Container(
-                    alignment: Alignment.topLeft,
-                    child: const Text(
-                      'New Plants',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-                ]
-            ),
-
-
-
+                ),
+              ),
                Container(
-                 margin: EdgeInsets.only(top:MediaQuery.of(context).size.width / 1.5,
-                     left: 20,right: 20),
-
-                 child: ListView.builder(
-                     scrollDirection: Axis.horizontal,
-                     itemCount: cart_lists.length,
-                     itemBuilder: (context , index){
-                      return  cartPlant(cart2: cart_lists[index]);
-
-                    }
-
-            ),
+                padding: const EdgeInsets.fromLTRB(0, 20, 150, 0),
+                 child: TextField(
+                  
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 240, 240, 240),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                      borderSide: BorderSide.none
+                    ),
+                    prefixIcon: Icon(Icons.search),
+                    hintText: 'Search...'
+                  ),
+              ),
                ),
-          ]
+              firstCart(context),
+              SizedBox(
+                height: MediaQuery.of(context).size.width / 11,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'New Plants',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ]),
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width / 1,
+                  ),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: cart_lists.length,
+                  itemBuilder: (context, index) {
+                    return cartPlant(cart2: cart_lists[index]);
+                  }),
+            ),
+          ]),
         ),
-
-
-      ),
-
-
-
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined, size: 25,color: Colors.black),
+              icon: Icon(Icons.home_outlined, size: 25, color: Colors.black),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline_sharp, size: 25,color: Colors.black),
+              icon: Icon(Icons.favorite_outline_sharp,
+                  size: 25, color: Colors.black),
               label: '',
             ),
-
             BottomNavigationBarItem(
-              icon: Icon(Icons.qr_code_scanner_outlined,size: 25),
+              icon: Icon(Icons.qr_code_scanner_outlined, size: 25),
               activeIcon: CircleAvatar(
-                  backgroundColor:Color.fromARGB(255, 144, 205, 141),
+                  backgroundColor: Color.fromARGB(255, 144, 205, 141),
                   radius: 20,
-                  child: Icon(Icons.qr_code_scanner_outlined,color: Colors.black,size: 25)),
+                  child: Icon(Icons.qr_code_scanner_outlined,
+                      color: Colors.black, size: 25)),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined, size: 22,color: Colors.black),
+              icon: Icon(Icons.shopping_bag_outlined,
+                  size: 22, color: Colors.black),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline_rounded, size: 25,color: Colors.black),
+              icon: Icon(Icons.person_outline_rounded,
+                  size: 25, color: Colors.black),
               label: '',
             ),
-
           ],
 
           type: BottomNavigationBarType.shifting,
@@ -112,61 +123,55 @@ class _home_pageState extends State<home_page> {
           onTap: _onItemTapped,
           // unselectedFontSize: 0,
           unselectedItemColor: Colors.black12,
-        )
-
-    );
+        ));
   }
 }
 
-
-
- firstCart(context){
-   return
-  Stack(
-     children: [
-       Container(
-         margin: EdgeInsets.only(top: 50),
-         height: 110.0,
-         width: double.infinity,
-         decoration: BoxDecoration(
-           color: Color.fromARGB(255, 144, 205, 141),
-           borderRadius: BorderRadius.circular(20),
-         ),
-       ),
-       Row(
-         children: [
-           Padding(
-             padding: const EdgeInsets.fromLTRB(50, 50, 0, 0),
-             child: Column(
-               children: const [
-                 Text(
-                   '30% OFF',
-                   style: TextStyle(
-                     fontSize: 20,
-                     fontWeight: FontWeight.bold,
-                   ),
-                 ),
-                 Text(
-                   '02-23 July',
-                   style: TextStyle(
-                       fontSize: 16, fontWeight: FontWeight.w300),
-                 ),
-               ],
-             ),
-           ),
-           SizedBox(
-             width: MediaQuery.of(context).size.width / 3.8,
-           ),
-           Container(
-             height: 150,
-             child: const Image(
-               image: AssetImage(
-                 'assest/images/plant1.png',
-               ),
-             ),
-           )
-         ],
-       ),
-     ],
-   );
- }
+firstCart(context) {
+  return Stack(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 40),
+        height: 110.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 144, 205, 141),
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(50, 50, 0, 0),
+            child: Column(
+              children: const [
+                Text(
+                  '30% OFF',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '02-23 July',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 3.1,
+          ),
+          Container(
+            height: 150,
+            child: const Image(
+              image: AssetImage(
+                'assest/images/plant1.png',
+              ),
+            ),
+          )
+        ],
+      ),
+    ],
+  );
+}
