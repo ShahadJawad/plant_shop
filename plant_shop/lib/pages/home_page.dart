ccf56cf7fa3f2c5158959abaa980989f8e544b22
AliBahaa <alibahaa963@gmail.com>
2022-10-29 17:21:01 +0300

    add

--- plant_shop/lib/pages/home_page.dart
+++ plant_shop/lib/pages/home_page.dart
@@ -20,6 +20,7 @@ class _home_pageState extends State<home_page> {
       _selectedIndex = index;
     });
   }
+
   @override
   //final user =FirebaseAuth.instance.currentUser!;
   Widget build(BuildContext context) {
@@ -50,37 +51,36 @@ class _home_pageState extends State<home_page> {
             ),
           ),
         ),
-
-
-
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined, size: 25,color: Colors.black),
+              icon: Icon(Icons.home_outlined, size: 25, color: Colors.black),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline_sharp, size: 25,color: Colors.black),
+              icon: Icon(Icons.favorite_outline_sharp,
+                  size: 25, color: Colors.black),
               label: '',
             ),
-
             BottomNavigationBarItem(
-              icon: Icon(Icons.qr_code_scanner_outlined,size: 25),
+              icon: Icon(Icons.qr_code_scanner_outlined, size: 25),
               activeIcon: CircleAvatar(
-                  backgroundColor:Color.fromARGB(255, 144, 205, 141),
+                  backgroundColor: Color.fromARGB(255, 144, 205, 141),
                   radius: 20,
-                  child: Icon(Icons.qr_code_scanner_outlined,color: Colors.black,size: 25)),
+                  child: Icon(Icons.qr_code_scanner_outlined,
+                      color: Colors.black, size: 25)),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined, size: 22,color: Colors.black),
+              icon: Icon(Icons.shopping_bag_outlined,
+                  size: 22, color: Colors.black),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline_rounded, size: 25,color: Colors.black),
+              icon: Icon(Icons.person_outline_rounded,
+                  size: 25, color: Colors.black),
               label: '',
             ),
-
           ],
 
           type: BottomNavigationBarType.shifting,
@@ -89,61 +89,55 @@ class _home_pageState extends State<home_page> {
           onTap: _onItemTapped,
           // unselectedFontSize: 0,
           unselectedItemColor: Colors.black12,
-        )
-
-    );
+        ));
   }
 }
 
-
-
-firstCart(context){
-  return
-    Stack(
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 50),
-          height: 110.0,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 144, 205, 141),
-            borderRadius: BorderRadius.circular(20),
-          ),
+firstCart(context) {
+  return Stack(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 50),
+        height: 110.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 144, 205, 141),
+          borderRadius: BorderRadius.circular(20),
         ),
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(50, 50, 0, 0),
-              child: Column(
-                children: const [
-                  Text(
-                    '30% OFF',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+      ),
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(50, 50, 0, 0),
+            child: Column(
+              children: const [
+                Text(
+                  '30% OFF',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    '02-23 July',
-                    style: TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w300),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 3.8,
-            ),
-            Container(
-              height: 150,
-              child: const Image(
-                image: AssetImage(
-                  'assest/images/plant1.png',
                 ),
+                Text(
+                  '02-23 July',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 3.8,
+          ),
+          Container(
+            height: 150,
+            child: const Image(
+              image: AssetImage(
+                'assest/images/plant1.png',
               ),
-            )
-          ],
-        ),
-      ],
-    );
+            ),
+          )
+        ],
+      ),
+    ],
+  );
 }
